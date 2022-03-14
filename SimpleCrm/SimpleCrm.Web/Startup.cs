f13ca6b875cf064fbc697996fe6de5d3ca844f31
Amber <amber@email.com>
2022-03-14 11:26:39 -0500

    Step004: Lesson Completed Code

--- SimpleCrm/SimpleCrm.Web/Startup.cs
+++ SimpleCrm/SimpleCrm.Web/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -12,11 +13,20 @@ namespace SimpleCrm.Web
 {
     public class Startup
     {
+       private readonly IConfiguration configuration;
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+
+        } 
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
         }
+
+        
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -32,7 +42,8 @@ namespace SimpleCrm.Web
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello Nexual Academy!");
+                    var greeting = configuration["Greeting"];
+                    await context.Response.WriteAsync(greeting); 
                 });
             });
         }
