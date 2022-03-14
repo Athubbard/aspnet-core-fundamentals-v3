4aad9f6f96b6517a600540f7c59e70c58657dcc9
Amber <amber@email.com>
2022-03-14 12:56:01 -0500

    Step005: ConfigurationGreeter

---
+++ SimpleCrm/SimpleCrm/ConfigurationGreeter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCrm
+{
+    public class ConfigurationGreeter : IGreeter
+    {
+        private readonly IConfiguration configuration;
+
+        public ConfigurationGreeter(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        public string GetGreeting()
+        {
+            var greeting = configuration["Greeting"];
+
+            return greeting;
+        
+        }
+    }
+}
