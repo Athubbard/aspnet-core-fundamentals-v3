68f87d68a5a97b110da5419f81d2fe032c328023
amber <amberthubbard@gmail.com>
2022-03-28 10:53:18 -0500

    Step12

--- SimpleCrm/SimpleCrm.Web/Controllers/AboutController.cs
+++ SimpleCrm/SimpleCrm.Web/Controllers/AboutController.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace SimpleCrm.Web.Controller
+namespace SimpleCrm.Web.Controllers
 {
     [Route("about")]
     public class AboutController
---
+++ SimpleCrm/SimpleCrm.Web/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleCrm.Web.Models;
+
+namespace SimpleCrm.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(string id)
+        {
+
+            var model = new CustomerModel {
+                Id=1, 
+                FirstName= "John", 
+                LastName="Doe",
+                PhoneNumber="314-123-4567" 
+            };
+            return new ObjectResult(model);
+        }
+    }
+}
