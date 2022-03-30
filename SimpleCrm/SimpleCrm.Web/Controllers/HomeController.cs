4bc8a1a11f7a139d709a9788656f479172592c3e
amber <amberthubbard@gmail.com>
2022-03-30 10:10:47 -0500

    Step15

--- SimpleCrm/SimpleCrm.Web/Controllers/HomeController.cs
+++ SimpleCrm/SimpleCrm.Web/Controllers/HomeController.cs
@@ -6,15 +6,22 @@ namespace SimpleCrm.Web.Controllers
     public class HomeController : Controller
     {
         private ICustomerData _customerData;
-        public HomeController(ICustomerData customerData)
+        private readonly IGreeter _greeter;
+
+        public HomeController(ICustomerData customerData, IGreeter greeter)
         {
             _customerData = customerData;
+            _greeter = greeter;
         }
         public IActionResult Index(string id)
         {
 
-            var model = _customerData.GetAll();
-                
+            var model = new HomePageViewModel
+            {
+                CurrentMessage = _greeter.GetGreeting(),
+                Customers = _customerData.GetAll()
+            };
+
             return View(model);
         }
     }
