bd790e4da6cf3e527ed533a78faa3e2ae885149c
amber <amberthubbard@gmail.com>
2022-04-04 23:07:33 -0500

    Step20

--- SimpleCrm/SimpleCrm.Web/Models/CustomerEditViewModel.cs
+++ SimpleCrm/SimpleCrm.Web/Models/CustomerEditViewModel.cs
@@ -1,6 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 
 namespace SimpleCrm.Web.Models
@@ -8,8 +9,18 @@ namespace SimpleCrm.Web.Models
     public class CustomerEditViewModel
     {
         public int Id { get; set; }
+        [Display(Name = "First Name")]
+
+        [Required, MinLength(1), MaxLength(30)]
         public string FirstName { get; set; }
+
+        [Required, MinLength(1), MaxLength(30)]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+
+        [Required, MinLength(1), MaxLength(10)]
+        [DataType (DataType.PhoneNumber)]
+        [Display(Name = "Phone")]
         public string PhoneNumber { get; set; }
         public bool OptInNewsletter { get; set; }
         public CustomerType Type { get; set; }
