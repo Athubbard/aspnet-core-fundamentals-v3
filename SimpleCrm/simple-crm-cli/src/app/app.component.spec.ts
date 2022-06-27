f4fb14c8dabddb96e90fe5db36fc558d57e1e64f
ahubbard01 <ahubbard@gatewaysolutions.com>
2022-06-27 12:40:22 -0500

    68: unit testing app component

--- SimpleCrm/simple-crm-cli/src/app/app.component.spec.ts
+++ SimpleCrm/simple-crm-cli/src/app/app.component.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
-describe('AppComponent', () => {
+fdescribe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
@@ -20,16 +20,30 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'simple-crm-cli'`, () => {
+  it(`should have as title 'Simple CRM'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('simple-crm-cli');
+    expect(app.title).toEqual('Simple Crm');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('simple-crm-cli app is running!');
+    expect(compiled.querySelector('span.title')?.textContent).toContain('Simple Crm');
   });
+});
+
+it('should render Customer Menu Item', () => {
+  const fixture = TestBed.createComponent(AppComponent);
+  fixture.detectChanges();
+  const compiled = fixture.nativeElement as HTMLElement;
+  expect(compiled.querySelector('h4.mat-line')?.textContent).toContain('Customer');
+});
+
+it('should render Pipe Menu Item line', () => {
+  const fixture = TestBed.createComponent(AppComponent);
+  fixture.detectChanges();
+  const compiled = fixture.nativeElement as HTMLElement;
+  expect(compiled.querySelector('h4.mat-line')?.textContent).toContain('PipeLine');
 });
