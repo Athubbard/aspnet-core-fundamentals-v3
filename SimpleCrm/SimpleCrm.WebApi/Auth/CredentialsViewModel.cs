96886fd1d23e39ba4dd86b04bdfa120f129d98a1
ahubbard01 <ahubbard@gatewaysolutions.com>
2023-03-13 11:09:27 -0500

    116: create AuthController and CredentialsViewModel

---
+++ SimpleCrm/SimpleCrm.WebApi/Auth/CredentialsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleCrm.WebApi.Auth
+{
+    public class CredentialsViewModel
+    {
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
+    }
+}
