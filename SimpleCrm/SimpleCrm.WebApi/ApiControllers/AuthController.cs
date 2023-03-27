7f842f8db56f4f4dbea03777b5ebb1e42bdbc56d
ahubbard01 <ahubbard@gatewaysolutions.com>
2023-03-27 11:04:32 -0500

    118: get user data with JWT for authenticated user

--- SimpleCrm/SimpleCrm.WebApi/ApiControllers/AuthController.cs
+++ SimpleCrm/SimpleCrm.WebApi/ApiControllers/AuthController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using SimpleCrm.WebApi.Auth;
 using System;
 using System.Collections.Generic;
@@ -10,6 +11,12 @@ namespace SimpleCrm.WebApi.ApiControllers
     [Route("api/auth")]
     public class AuthController : Controller
     {
+        private readonly UserManager<CrmUser> userManager;
+        private readonly JwtFactory _jwtFactory;
+        public AuthController(UserManager<CrmUser> userManager)
+        {
+            this.userManager = userManager;
+        }
         [HttpPost("login")]
         public async Task<IActionResult> Post([FromBody] CredentialsViewModel credentials)
         { // TODO: create a CredentialsViewModel class in the next assignment
@@ -17,19 +24,29 @@ namespace SimpleCrm.WebApi.ApiControllers
             {
                 return UnprocessableEntity(ModelState);
             }
-           
-            // TODO: add Authenticate method (see lesson below)
-            var user = await Authenticate(credentials.EmailAddress, credentials.Password);
+
+            var user = await userManager.FindByEmailAsync(credentials.EmailAddress);
             if (user == null)
             {
                 return UnprocessableEntity("Invalid username or password.");
             }
-
-            // TODO: add GetUserData method (see lesson below)
+            var validPassword = await userManager.CheckPasswordAsync(user, credentials.Password);
             var userModel = await GetUserData(user);
-            // returns a UserSummaryViewModel containing a JWT and other user info
             return Ok(userModel);
+
+
         }
+
+       
+      /* public async Task<UserSummaryViewModel> GetUserData(CrmUser crmUser)
+        {
+            // TODO: add GetUserData method (see lesson below)
+            var jwt = await _jwtFactory.GenerateEncodedToken(crmUser.UserName,
+       _jwtFactory.GenerateClaimsIdentity(crmUser.UserName, crmUser.Id.ToString()));
+            
+        //returns a UserSummaryViewModel containing a JWT and other user info
+       return Ok(userModel); 
+       //}*/
     }
 }
 
---
+++ SimpleCrm/SimpleCrm.WebApi/Auth/UserSummaryViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace SimpleCrm.WebApi.Auth
+{
+    public class UserSummaryViewModel
+    {
+        public string User { get; set; }
+        public string Id { get; set; }
+        public string name { get; set; }
+        public string Emailaddress { get; set; }
+        public string roles { get; set; }
+        public string JWTtoken { get; set; }
+
+    }
+}
